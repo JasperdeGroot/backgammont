ac50434ffceadc148d7d319049ca75719a396432
Jasper de Groot <148417@helenparkhurst.net>
2016-06-08 14:05:44 +0200

    Geprobeerd de server online te krijgen maar helaas niet gelukt...

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $naam = filter_input(INPUT_POST, 'naam');
 $alias = filter_input(INPUT_POST, 'alias');
 
 $DB_host = "v14jgroot.helenparkhurst.net";
-$DB_name = "backgammon";
+$DB_name = "v14jgroot_leden";
 $DB_user = "backgammon"; //er stond eerst root maar weet niet echt wat dat inhoud.
 $DB_pass = "1q2w3e4r";
 
